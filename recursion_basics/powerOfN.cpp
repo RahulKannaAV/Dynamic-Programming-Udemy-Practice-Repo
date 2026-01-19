9205c736927a642ee0d7877777fa2828f9bb1e31
Nickobellic <avrahulkanna17@gmail.com>
2026-01-19 19:25:08 +0530

    Update Power of X with log steps based approach

--- recursion_basics/powerOfN.cpp
+++ recursion_basics/powerOfN.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // a^n = a * a^n-1
 // a^n-1 is the subproblem
-
+// Takes O(N) TC & SC
 int powerOfN(int base, int power) {
     if(power <= 0) {
         return 1;
@@ -13,9 +13,32 @@ int powerOfN(int base, int power) {
     return result;
 }
 
+// Optimized way: a^n = (a^(n/2))^2 if 'n' is even
+// a^n = a * (a^(n/2))^2 if 'n' is odd
+// only takes log2(power) steps
+int optimizedPowerOfN(int base, int power) {
+    // Base case
+    if(power == 0) {
+        return 1;
+    }
+
+    int recCase = optimizedPowerOfN(base, power/2);
+
+    // Power Odd Even Recursive Case
+    if(power%2 == 0) {
+        // a^(n/2) * a^(n/2)
+        return recCase * recCase;
+    } else {
+        // a * a^(n/2) * a^(n/2)
+        return base * recCase * recCase;
+    }
+}
+
 int main() {
     int base = 2;
     int power = 10;
 
     cout << powerOfN(base, power) << endl;
+    cout << "Optimized Way Answer: " << optimizedPowerOfN(base, power) << endl;
+    return 0;
 }
